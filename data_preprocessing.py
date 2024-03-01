b23b42d11f5a02f452e85c5c03ad0b59873587c3
Vicz99 <159501989+Vicz99@users.noreply.github.com>
2024-03-01 04:26:46 -0500

    cleaned up the code

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,6 +9,7 @@ df.columns = df.columns.str.strip()
 # remove leading and trailing spaces from all string values in df
 df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
+
 # deal with missing value
 # drop 10 countries without life expectancy 
 rows_with_nan = df[df['Life expectancy'].isna()].index
@@ -20,6 +21,7 @@ na_cols = [col for col in df.columns if df[col].isna().any()]
 for country in countries:
     # interpolate missing values for each country
     df.loc[df['Country'] == country, na_cols] = df.loc[df['Country'] == country, na_cols].interpolate()
+
 
 # For countries with more than 8 missing years for any column, we choose not to analyze this country for corresponding columns.
 # Function to check and report countries with more than 8 missing years for any column
@@ -51,36 +53,31 @@ pickle_file_path = "missing_values_report.pkl"
 with open(pickle_file_path, 'wb') as pickle_file:
     pickle.dump(report, pickle_file)
 print(f"Report saved to {pickle_file_path}")
-
-# add column country code as mapping to Country
-country_names = df['Country'].unique()
-
-# get the ISO Alpha-3 code for a given country name
-def get_country_code(country_name):
-    try:
-        return pycountry.countries.lookup(country_name).alpha_3
-    except LookupError:
-        # Handle cases where the country name does not match exactly
-        # Implement any specific name adjustments here if necessary
-        return None
-
-# Map each country name to its ISO Alpha-3 code
-country_codes = [get_country_code(name) for name in country_names]
-
-# Create a DataFrame from the mapping
-country_code_mapping = pd.DataFrame({
-    'Country': country_names,
-    'country-code': country_codes
-})
-
-# Merge this mapping back into your original DataFrame
-df = df.merge(country_code_mapping, on='Country', how='left')
-
 # Load the dictionary back from the pickle file
 #with open(pickle_file_path, 'rb') as pickle_file:
 #    loaded_report = pickle.load(pickle_file)
 
 #print(loaded_report)
+
+
+# add column country code as mapping to Country
+country_names = df['Country'].unique()
+# get the ISO Alpha-3 code for a given country name
+def get_country_code(country_name):
+    try:
+        return pycountry.countries.lookup(country_name).alpha_3
+    except LookupError:
+        # if any error
+        return None
+
+# map each country name to its ISO Alpha-3 code
+country_codes = [get_country_code(name) for name in country_names]
+country_code_mapping = pd.DataFrame({
+    'Country': country_names,
+    'country-code': country_codes
+})
+# merge this mapping back into original DataFrame
+df = df.merge(country_code_mapping, on='Country', how='left')
 
 df.to_csv('life_expectancy_clean.csv')
 
