f24a3e8b40a09fd5ec5e9081ff520053c7f7345c
Vicz99 <159501989+Vicz99@users.noreply.github.com>
2024-03-01 19:48:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
 # streamlit app code file
+import streamlit
+from multiapp import MultiApp
+from apps import streamlit_task1, streamlit_task2_4
+#from apps import streamlit_task1, streamlit_task2_4, smiles_viz
+
+
+app = MultiApp()
+
+app.add_app("Task 1", streamlit_task1.app)
+app.add_app("Task 2 & 4", streamlit_task2_4.app)
+#app.add_app("Task 3", streamlit_task3.app)
+# The main app
+app.run()
