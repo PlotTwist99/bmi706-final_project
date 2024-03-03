3a6aba24b99a000c332dc358e46d7dc86cbb2c11
Doug Snyder <doug0x2a@gmail.com>
2024-03-03 08:51:31 -0500

    Link charts and add interactions

--- apps/streamlit_task2_4.py
+++ apps/streamlit_task2_4.py
@@ -11,105 +11,133 @@ def load_data():
 
 def app():
     life_expectancy = load_data()
-    min_year = life_expectancy.Year.min()
-    max_year = life_expectancy.Year.max()
 
-    top_col1, top_col2 = st.columns([1, 5])
+    top_col1, top_col2, top_col3, top_col4 = st.columns([2.5, 1, 2.5, 1])
 
-    with top_col1:
-        toggle_option = st.radio(
-            "Choose:",
-            ('Year', 'Countries')
-        )
-
-    with top_col2:
-        if toggle_option == 'Year':
-            year = st.slider('Select a Year', min_value=min_year, max_value=max_year, value=2010)
-        elif toggle_option == 'Countries':
-            countries = st.multiselect('Select Countries', life_expectancy['Country'].unique(),
-                                    default=['Canada', 'China'])
-
-    if toggle_option == 'Year':
-        st.write(f"Displaying data for the year: {year}")
-        pass
-    elif toggle_option == 'Countries':
-        st.write(f"Displaying data for countries: {', '.join(countries)}")
-
-
-    bottom_col1, bottom_col2 = st.columns([1, 4])
+    default_year = 2012
+    default_countries = ['Canada', 'China']
 
     axis_variables = ['Life expectancy', 'Adult Mortality',
-        'infant deaths', 'Alcohol', 'percentage expenditure', 'Hepatitis B',
+        'infant deaths', 'Alcohol', 'Hepatitis B',
         'Measles', 'BMI', 'under-five deaths', 'Polio', 'Total expenditure',
         'Diphtheria', 'HIV/AIDS', 'GDP', 'Population',
         'Income composition of resources', 'Schooling']
 
-    with bottom_col1:
+    with top_col1:
         # Y axis variables
         y_variable = st.selectbox(
             "Y-axis:",
             axis_variables, index=axis_variables.index('Life expectancy')
         )
-        y_scale = st.radio("Y-axis scale:", ('linear', 'log'))
 
-        # X variable will be x axis or size depending on chart type.
-        x_name = "X-axis" if toggle_option == 'Year' else "Size"
+    with top_col2:
+        y_scale = st.radio("Scale", ('linear', 'log'), key='y-scale', horizontal=False)
+
+    with top_col3:
         x_variable = st.selectbox(
-            f"{x_name}:",
-            axis_variables, index=axis_variables.index('GDP')
+            "X-axis:",
+            axis_variables, index=axis_variables.index('Total expenditure')
         )
-        x_scale = st.radio(f"{x_name} scale:", ('linear', 'log'))
 
-        if toggle_option == 'Year':
-            pass
-        elif toggle_option == 'Countries':
-            pass
+    with top_col4:
+        x_scale = st.radio("Scale", ('linear', 'log'), key='x-scale', horizontal=False, index=0)
 
-    with bottom_col2:
-        if toggle_option == 'Year':
-            base_chart = alt.Chart(life_expectancy[life_expectancy.Year==year])
+    # Charts
+    chart_width = 600
+    chart_height = 380
+    country_selection = alt.selection_multi(fields=['Country'], empty='none', on='click',
+                                             toggle='true', init=[{'Country': country} for country in default_countries])
+    year_selection = alt.selection_single(fields=['Year'], empty='none', on='click', init={'Year': default_year})
 
-            # Create the base scatter plot
-            scatter_plot = base_chart.mark_point(filled=True).encode(
-                x=alt.X(x_variable, type='quantitative', scale=alt.Scale(type=x_scale, zero=False)),
-                y=alt.Y(y_variable, type='quantitative', scale=alt.Scale(type=y_scale, zero=False)),
-                    tooltip=[
-                    alt.Tooltip('Country:N'),
-                    alt.Tooltip(f'{y_variable}:Q'),
-                    alt.Tooltip(f'{x_variable}:Q')
-                    ]
-            )
+    top_base = alt.Chart(life_expectancy).properties(width=chart_width, height=chart_height)
 
-            if y_scale == x_scale:
-                trend_method = 'linear'
-            elif y_scale == 'linear' and x_scale == 'log':
-                trend_method = 'log'
-            elif y_scale == 'log' and x_scale == 'linear':
-                trend_method = 'exp'
-            else:
-                trend_method = 'linear' # shouldn't get here.
-            trend_line = scatter_plot.transform_regression(
-                x_variable, y_variable, method=trend_method
-            ).mark_line(color='red')
+    scatter_plot = top_base.mark_point(filled=True).encode(
+        x=alt.X(x_variable, type='quantitative', scale=alt.Scale(type=x_scale, zero=False)),
+        y=alt.Y(y_variable, type='quantitative', scale=alt.Scale(type=y_scale, zero=False)),
+        tooltip=[
+            alt.Tooltip('Country:N'),
+            alt.Tooltip(f'{y_variable}:Q'),
+            alt.Tooltip(f'{x_variable}:Q')
+        ]
+    )
 
+    if y_scale == x_scale:
+        trend_method = 'linear'
+    elif y_scale == 'linear' and x_scale == 'log':
+        trend_method = 'log'
+    elif y_scale == 'log' and x_scale == 'linear':
+        trend_method = 'exp'
+    else:
+        trend_method = 'linear'  # shouldn't get here.
 
-            chart = scatter_plot + trend_line
+    trend_line = scatter_plot.transform_regression(
+        x_variable, y_variable, method=trend_method
+    ).mark_line(color='red')
 
-            # Display the chart
-            st.altair_chart(chart, use_container_width=True)
-        elif toggle_option == 'Countries':
-            base_chart = alt.Chart(life_expectancy[life_expectancy['Country'].isin(countries)])
-            line_chart = base_chart.mark_point(filled=True).encode(
-                x='Year:O',
-                y=alt.Y(y_variable, type='quantitative', scale=alt.Scale(type=y_scale, zero=False)),
-                #order='Year:O',
-                color='Country:N',
-                size=alt.Size(x_variable, type='quantitative', scale=alt.Scale(type=x_scale)),
-                tooltip=[
-                alt.Tooltip('Country:N'),
-                alt.Tooltip(f'{y_variable}:Q'),
-                alt.Tooltip(f'{x_variable}:Q'),
-                alt.Tooltip('Year:Q')
-                ]
-            )
-            st.altair_chart(line_chart, use_container_width=True)     
+    # Year Text:
+    years_df = pd.DataFrame({
+        'Year': range(life_expectancy['Year'].min(), life_expectancy['Year'].max() + 1),
+        'LabelPosition': 1
+    })
+
+    year_text = alt.Chart(years_df).mark_text(
+        align='right',
+        baseline='top',
+        dx=-5,
+        dy=-5
+    ).encode(
+        x=alt.value(40),
+        y=alt.value(8),
+        text=alt.Text('Year:N'),
+        color=alt.value('blue'),
+        size=alt.value(16)
+    ).transform_filter(
+        year_selection
+    )
+
+    # Text Labels to selected countries
+    text_labels = top_base.mark_text(align='left', dx=5, dy=-5).encode(
+        x=alt.X(x_variable, type='quantitative', scale=alt.Scale(type=x_scale, zero=False)),
+        y=alt.Y(y_variable, type='quantitative', scale=alt.Scale(type=y_scale, zero=False)),
+        text='Country:N',
+        color=alt.condition(country_selection, alt.value('black'), alt.value('transparent'))  # Make text transparent if not selected
+    ).transform_filter(
+        country_selection  # Apply the selection to filter labels
+    ).transform_filter(
+        year_selection
+    )
+
+    scatter_plot = scatter_plot.encode(
+        color=alt.condition(country_selection, alt.value('red'), alt.value('steelblue')),
+        size=alt.condition(country_selection, alt.value(100), alt.value(30))
+    ).add_selection(
+        country_selection
+        # Note: year_selection is not added here since it's intended to be modified by another chart
+    ).add_selection(year_selection).transform_filter(year_selection)
+
+    top_chart = alt.layer(scatter_plot, trend_line, text_labels, year_text)
+
+    # Display the chart (assuming you're using Streamlit or similar)
+    # st.altair_chart(top_chart, use_container_width=True)
+    #top_chart
+
+    bottom_base = alt.Chart(life_expectancy).properties(width=chart_width, height=chart_height)
+    line_chart = bottom_base.mark_point(filled=True).encode(
+        x='Year:O',
+        y=alt.Y(y_variable, type='quantitative', scale=alt.Scale(type=y_scale, zero=False)),
+        #order='Year:O',
+        color='Country:N',
+        size=alt.Size(x_variable, type='quantitative', scale=alt.Scale(type=x_scale)),
+        tooltip=[
+        alt.Tooltip('Country:N'),
+        alt.Tooltip(f'{y_variable}:Q'),
+        alt.Tooltip(f'{x_variable}:Q'),
+        alt.Tooltip('Year:Q')
+        ]
+    ).add_selection(country_selection).transform_filter(country_selection).add_selection(year_selection)
+
+    chart = alt.vconcat(top_chart, line_chart).resolve_scale(
+        color='independent',
+        size='independent'
+    )
+    st.altair_chart(chart, use_container_width=False)
